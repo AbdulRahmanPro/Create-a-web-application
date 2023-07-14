94029f71c4fdba8884a86c8ca0101f9669bac4a5
AbdulRahmanPro <progammerabood@gmail.com>
2023-07-14 11:36:53 +0300

    تأكد من الكود ولاتظهر اي مشكلة

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const path = require('path');
 const multer = require("multer");
 const Product = require('../models/products');
 const fs = require('fs-extra');
+const mongoose = require('mongoose');   
 // متغيرات لانشاء المخطط
 
 
@@ -33,10 +34,59 @@ const upload = multer({
 }); // تحديد اسم الصورة
 
 /* GET users listing. */
-router.put('/editItem/:id', (req, res) => {
+
+const { body, param, validationResult } = require('express-validator');
+
+router.put(
+  '/editItem/:id',
+  [
+    param('id').isMongoId().withMessage('Invalid ID format'),
+    body('name_type').notEmpty().withMessage('Name type is required'),
+    body('value').notEmpty().withMessage('Value is required')
+  ],
+  async (req, res) => {
     const id = req.params.id;
-    console.log(412432);
-});
+    const { name_type, value } = req.body;
+
+    // التحقق من وجود أخطاء في البيانات
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const updateFields = {
+        [name_type]: value
+      };
+
+      // التحقق من صحة المعرف
+      const isValidId = mongoose.isValidObjectId(id);
+      if (!isValidId) {
+        return res.status(400).json({ error: 'Invalid ID' });
+      }
+      if (name_type == "tag" ){
+        if (value == "متوفر" && value == "غير متوفر" ){
+
+            const result = await Product.findByIdAndUpdate(id, updateFields);
+            console.log(result);
+            if (!result) {
+                return res.status(404).json({ error: 'Product not found' });
+            }
+            
+      }
+
+     }
+        const result = await Product.findByIdAndUpdate(id, updateFields);
+        console.log(result);
+        
+      res.redirect('/admin');
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+);
+
 
 router.delete('/deleteItem/:id', (req, res) => {
     const id = req.params.id;
